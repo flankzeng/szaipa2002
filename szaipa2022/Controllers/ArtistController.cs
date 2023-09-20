6d7065f0bfc39c90d469f03d00d8a7bf33ddde4c
ArthurADDDDD <a535483659@gmail.com>
2023-09-20 17:26:58 +0800

    ✨ feat(控制器与文件夹): 新增了Artist版块的首页

---
+++ szaipa2022/Controllers/ArtistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Szaipa.Controllers
+{
+    public class ArtistController : Controller
+    {
+        // GET: Publication
+        public ActionResult index()//艺术家官网
+        {
+            return View();
+        }
+    }
+}
